85ee56901af271f2aee3d39f8d712c1becec9d03
Sourendu <sourendulodh@gmail.com>
2024-07-21 20:12:14 +0530

    final

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConfirmedBooking = () => {
+    return(
+        <header>
+            <section>
+            <h1>Booking has been confirmed Thank you!
+        </h1>
+            </section>
+        </header>
+
+    )
+}
+
+export default ConfirmedBooking;
